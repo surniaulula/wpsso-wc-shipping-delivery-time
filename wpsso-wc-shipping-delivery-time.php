2a2f4fd075eab151ff6da6f1bccde19d26d14e03
JS Morisset <jsm@surniaulula.com>
2020-10-18 11:17:54 -0700

    v1.0.1

--- wpsso-wc-shipping-delivery-time.php
+++ wpsso-wc-shipping-delivery-time.php
@@ -68,7 +68,10 @@ if ( ! class_exists( 'WpssoWcsdt' ) ) {
 			load_plugin_textdomain( 'wpsso-wc-shipping-delivery-time', false, 'wpsso-wc-shipping-delivery-time/languages/' );
 		}
 
-		public function init_objects( $is_admin ) {
+		/**
+		 * $is_admin, $doing_ajax, and $doing_cron available since WPSSO Core v8.8.0.
+		 */
+		public function init_objects( $is_admin = false, $doing_ajax = false, $doing_cron = false ) {
 
 			$this->p =& Wpsso::get_instance();
 
