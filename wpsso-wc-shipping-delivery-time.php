52017d0b7a2d8012d03b1a961b8b79d71d3ceb5c
JS Morisset <jsm@surniaulula.com>
2020-10-20 12:11:21 -0700

    v2.0.0-dev.2

--- wpsso-wc-shipping-delivery-time.php
+++ wpsso-wc-shipping-delivery-time.php
@@ -15,7 +15,7 @@
  * Requires At Least: 4.4
  * Tested Up To: 5.5.1
  * WC Tested Up To: 4.6.0
- * Version: 1.0.1
+ * Version: 2.0.0-dev.2
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
