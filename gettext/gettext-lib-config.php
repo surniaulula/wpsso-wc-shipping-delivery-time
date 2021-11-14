b3d52c6f98964be92f3a7e0b9aab1294ef693ef6
JS Morisset <jsm@surniaulula.com>
2021-11-13 20:36:59 -0800

    v2.4.1-dev.3

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -6,3 +6,5 @@
 _x( 'WooCommerce Shipping', 'lib file description', 'wpsso-wc-shipping-delivery-time' );
 _x( 'WPSSO Shipping Delivery Time for WooCommerce SEO', 'plugin name', 'wpsso-wc-shipping-delivery-time' );
 _x( 'Shipping delivery time estimates for WooCommerce shipping zones, methods, and classes.', 'plugin description', 'wpsso-wc-shipping-delivery-time' );
+_x( 'WooCommerce', 'plugin name', 'wpsso-wc-shipping-delivery-time' );
+_x( 'WPSSO Core', 'plugin name', 'wpsso-wc-shipping-delivery-time' );
