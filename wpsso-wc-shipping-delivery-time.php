514c4f61e180c96626bf10ebc6d5bd7c6924f56e
JS Morisset <jsm@surniaulula.com>
2022-08-23 10:19:46 -0700

    v2.7.1

--- wpsso-wc-shipping-delivery-time.php
+++ wpsso-wc-shipping-delivery-time.php
@@ -13,7 +13,7 @@
  * Description: Shipping delivery time estimates for WooCommerce shipping zones, methods, and classes.
  * Requires PHP: 7.2
  * Requires At Least: 5.2
- * Tested Up To: 6.0.1
+ * Tested Up To: 6.0.2
  * WC Tested Up To: 6.8.2
  * Version: 2.7.1
  *
