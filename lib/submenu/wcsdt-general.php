c0ac95145b46dae1f033f3a8505521a8796d5a3d
JS Morisset <jsm@surniaulula.com>
2022-12-29 09:29:04 -0800

    svn fix

---
+++ lib/submenu/wcsdt-general.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl.txt
+ * Copyright 2020-2022 Jean-Sebastien Morisset (https://wpsso.com/)
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+
+	die( 'These aren\'t the droids you\'re looking for.' );
+}
+
+if ( ! class_exists( 'WpssoWcsdtSubmenuWcsdtGeneral' ) && class_exists( 'WpssoAdmin' ) ) {
+
+	class WpssoWcsdtSubmenuWcsdtGeneral extends WpssoAdmin {
+
+		public function __construct( &$plugin, $id, $name, $lib, $ext ) {
+
+			$this->p =& $plugin;
+
+			if ( $this->p->debug->enabled ) {
+
+				$this->p->debug->mark();
+			}
+
+			$this->menu_id   = $id;
+			$this->menu_name = $name;
+			$this->menu_lib  = $lib;
+			$this->menu_ext  = $ext;
+		}
+
+		public function show_setting_page() {
+
+			$admin_url = admin_url( 'admin.php?page=wc-settings&tab=shipping&section=wcsdt' );
+
+			wp_redirect( $admin_url );
+		}
+	}
+}
