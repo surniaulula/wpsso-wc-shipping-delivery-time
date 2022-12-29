e2b896c5884b2e699c8adf988e885afab90566d8
JS Morisset <jsm@surniaulula.com>
2022-12-29 09:28:18 -0800

    svn fix

---
+++ gettext/gettext-lib-config.php
@@ -0,0 +1,9 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+// 
+// Extract of translatable strings from lib/config.php.
+// 
+_x( 'WC Shipping', 'lib file description', 'wpsso-wc-shipping-delivery-time' );
+_x( 'WPSSO Shipping Delivery Time for WooCommerce SEO', 'plugin name', 'wpsso-wc-shipping-delivery-time' );
+_x( 'Shipping delivery time estimates for WooCommerce shipping zones, methods, and classes.', 'plugin description', 'wpsso-wc-shipping-delivery-time' );
+_x( 'WPSSO Core', 'plugin name', 'wpsso-wc-shipping-delivery-time' );
+_x( 'WooCommerce', 'plugin name', 'wpsso-wc-shipping-delivery-time' );
