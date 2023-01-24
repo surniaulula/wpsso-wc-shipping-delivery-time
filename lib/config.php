ef6f8110ba0b36991cbd46ba0c1b13840d95d619
JS Morisset <jsm@surniaulula.com>
2023-01-24 06:30:27 -0800

    v2.8.1-dev.2

--- lib/config.php
+++ lib/config.php
@@ -70,6 +70,15 @@ if ( ! class_exists( 'WpssoWcsdtConfig' ) ) {
 							'wcsdt-general' => 'WC Shipping',
 						),
 					),
+
+					/*
+					 * Declare compatibility with WooCommerce HPOS.
+					 *
+					 * See https://github.com/woocommerce/woocommerce/wiki/High-Performance-Order-Storage-Upgrade-Recipe-Book.
+					 */
+					'wc_compat' => array(
+						'custom_order_tables',
+					),
 				),
 			),
 		);
