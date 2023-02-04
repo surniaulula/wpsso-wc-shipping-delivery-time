d286e439dd08ff1084c9a6f97745c032ef806811
JS Morisset <jsm@surniaulula.com>
2023-02-04 08:13:41 -0800

    v2.8.1

--- wpsso-wc-shipping-delivery-time.php
+++ wpsso-wc-shipping-delivery-time.php
@@ -69,6 +69,9 @@ if ( ! class_exists( 'WpssoWcsdt' ) ) {
 			load_plugin_textdomain( 'wpsso-wc-shipping-delivery-time', false, 'wpsso-wc-shipping-delivery-time/languages/' );
 		}
 
+		/*
+		 * Called by Wpsso->set_objects which runs at init priority 10.
+		 */
 		public function init_objects() {
 
 			$this->p =& Wpsso::get_instance();
