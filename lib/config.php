bce89f57fdba06fa2844b2d5989148687ba86c1c
JS Morisset <jsm@surniaulula.com>
2023-11-05 18:31:32 -0800

    v3.0.0-dev.14

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoWcsdtConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssowcsdt' => array(			// Plugin acronym.
-					'version'     => '3.0.0-dev.13',	// Plugin version.
+					'version'     => '3.0.0-dev.14',	// Plugin version.
 					'opt_version' => '1',		// Increment when changing default option values.
 					'short'       => 'WPSSO WCSDT',	// Short plugin name.
 					'name'        => 'WPSSO Shipping Delivery Time for WooCommerce SEO',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoWcsdtConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '17.0.0-dev.13',
+							'min_version'   => '17.0.0-dev.14',
 						),
 						'woocommerce' => array(
 							'name'          => 'WooCommerce',
