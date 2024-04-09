a31370bde3b8de4ceb7f78da6abd152ddee308fb
jsmoriss <jsm@surniaulula.com>
2024-04-09 22:42:17 +0000

    v3.1.0

--- wpsso-wc-shipping-delivery-time.php
+++ wpsso-wc-shipping-delivery-time.php
@@ -14,7 +14,7 @@
  * Requires Plugins: wpsso, woocommerce
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.5.0
+ * Tested Up To: 6.5.2
  * WC Tested Up To: 8.7.0
  * Version: 3.1.0
  *
