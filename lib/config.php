93379839f2fd25573b71a68d65aa683558e37f16
jsmoriss <jsm@surniaulula.com>
2024-09-16 13:39:28 +0000

    v3.2.0

--- lib/config.php
+++ lib/config.php
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoWcsdtConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '18.7.3',
+							'min_version'   => '18.8.0',
 						),
 						'woocommerce' => array(
 							'name'          => 'WooCommerce',
