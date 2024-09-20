a20f03b5d3fbf30e6e498a934cbc416ed9776bea
jsmoriss <jsm@surniaulula.com>
2024-09-20 20:39:08 +0000

    v3.2.0

--- lib/register.php
+++ lib/register.php
@@ -101,17 +101,14 @@ if ( ! class_exists( 'WpssoWcsdtRegister' ) ) {
 
 		private function activate_plugin() {
 
-			if ( class_exists( 'Wpsso' ) ) {
+			/*
+			 * Register plugin install, activation, update times.
+			 */
+			if ( class_exists( 'WpssoUtilReg' ) ) {
 
-				/*
-				 * Register plugin install, activation, update times.
-				 */
-				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO Core v6.13.1.
+				$version = WpssoWcsdtConfig::get_version();
 
-					$version = WpssoWcsdtConfig::get_version();
-
-					WpssoUtilReg::update_ext_version( 'wpssowcsdt', $version );
-				}
+				WpssoUtilReg::update_ext_version( 'wpssowcsdt', $version );
 			}
 		}
 
